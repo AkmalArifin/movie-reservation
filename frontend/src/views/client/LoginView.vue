015c3c147ff969fa04322b47479c81c9859afe6a
Muhammad Akmal Arifin <makmalarifin25@gmail.com>
2025-01-18 11:34:48 +0700

    feat: refactoring

---
+++ frontend/src/views/client/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Login View</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<script lang="ts">
+export default {
+    name: "LoginView"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/client/RegisterView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1> Register View</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<script lang="ts">
+export default {
+    name: "RegisterView"
+}
+</script>
+
+<style scoped>
+
+</style>
