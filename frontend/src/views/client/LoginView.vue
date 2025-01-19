d1660874f5ca4c918889d0ecd7a915765929758f
Muhammad Akmal Arifin <makmalarifin25@gmail.com>
2025-01-19 20:42:28 +0700

    feat: simple login page

--- frontend/src/views/client/LoginView.vue
+++ frontend/src/views/client/LoginView.vue
@@ -1,10 +1,28 @@
 <template>
-    <div>
+    <div class="body">
         <h1>Login View</h1>
+        <div class="input">
+            <input type="text" v-model="userInput.email">
+            <input type="password" v-model="userInput.password">
+            <button @click="handleLogin">Login</button>
+        </div>
+
     </div>
 </template>
 
 <script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+const userInput = reactive({
+    email: "",
+    password: ""
+})
+
+async function handleLogin(event:Event) {
+    event.preventDefault()
+
+    console.log(userInput)
+}
 
 </script>
 
