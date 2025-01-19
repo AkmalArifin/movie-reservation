ba24e311233e721b8f337898de76b1791c46ccdb
Muhammad Akmal Arifin <makmalarifin25@gmail.com>
2025-01-19 20:46:42 +0700

    feat: simple register

--- frontend/src/views/client/RegisterView.vue
+++ frontend/src/views/client/RegisterView.vue
@@ -1,11 +1,29 @@
 <template>
-    <div>
-        <h1> Register View</h1>
+    <div class="body">
+        <div class="input">
+            <input type="text" name="name" id="name" v-model="userInput.name">
+            <input type="text" name="email" id="email" v-model="userInput.email">
+            <input type="password" name="password" id="password" v-model="userInput.password">
+            <input type="password" name="confirmPassword" id="confirmPassword" v-model="userInput.confirmPassword">
+            <button @click="handleRegister">Sign Up</button>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { reactive } from 'vue'
+const userInput = reactive({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+})
 
+async function handleRegister(event:Event) {
+    event.preventDefault();
+
+    console.log(userInput);
+}
 </script>
 
 <script lang="ts">
