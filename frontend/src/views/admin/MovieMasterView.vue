3202ec6914b0840dd58aa7b4b185fe80a109a23b
Muhammad Akmal Arifin <makmalarifin25@gmail.com>
2025-01-22 10:49:22 +0700

    feat: authentication

--- frontend/src/views/admin/MovieMasterView.vue
+++ frontend/src/views/admin/MovieMasterView.vue
@@ -5,6 +5,53 @@
 </template>
 
 <script setup lang="ts">
+import axios from 'axios';
+import { useRouter } from 'vue-router'; 
+import { parseJwt } from '@/utils/jwt';
+
+const router = useRouter();
+
+const parsedToken = parseJwt(sessionStorage.getItem("token"));
+const NOW = Math.floor(Date.now() / 1000);
+
+if (parsedToken.exp < NOW) {
+    try {
+        const data = axios.post('http://localhost:8080/refresh', {}, { withCredentials: true })
+            .then(response => {
+                const data: {
+                    message: string,
+                    token: string
+                } = response.data;
+
+                sessionStorage.setItem("token", data.token);
+                const newParsedToken = parseJwt(data.token)
+                if (newParsedToken.role !== 'admin') {
+                    alert('not authorized');
+                    router.push({ name: 'login' });
+                }
+            })
+
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.log(error.message);
+        } else {
+            console.log(error);
+        }
+
+        alert('Please login first');
+        router.push({ name: 'login' });
+    }
+}
+
+if (parsedToken.role !== 'admin') {
+    alert('not authorized');
+    router.push({ name: 'login' });
+}
+
+// TODO: get data for table
+async function getData() {
+    axios.get('http://localhost:8080/movies')
+}
 
 </script>
 
